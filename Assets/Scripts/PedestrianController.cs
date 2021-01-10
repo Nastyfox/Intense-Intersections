2430d39159570a9285db78ac7b766d211052b9be
Nastyfox <julien.turbellier@gmail.com>
2021-01-11 00:40:34 +0100

    Adding streets textures and create road elements, spawning pedestrians at predefined positions, finishing adding colliders and rigidbody, make pedestrians move

---
+++ Assets/Scripts/PedestrianController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianController : MonoBehaviour
+{
+    //Private variables
+    private float speed = 2.5f;
+
+    //Private variables for positions outside map
+    private float horizontalOutside = 15.0f;
+    private float verticalOutside = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make car move forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        DestroyOutsideRoad();
+    }
+
+    private void DestroyOutsideRoad()
+    {
+        if (transform.position.x <= -horizontalOutside || transform.position.x >= horizontalOutside || transform.position.z <= -verticalOutside || transform.position.z >= verticalOutside)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,26 @@ public class SpawnManager : MonoBehaviour
 {
     //Private variables for spawning cars
     private float delayStart = 2.0f;
-    private float repeatingTime = 5.0f;
+    private float carRepeatingTime = 2.5f;
+    private float pedRepeatingTime = 2.5f;
     private int randomCarStart;
-    private int numPositions = 4;
-    private Vector3[] startPositions;
-    private Quaternion[] startRotations;
+    private int randomPedStart;
+    private int randomRotation;
+    private int carNumPositions = 4;
+    private int pedNumPositions = 8;
+    private int numRotations = 4;
+    private Vector3[] carStartPositions;
+    private Quaternion[] startRotations;  
+    private Vector3[] pedStartPositions;
     private Vector3 carPosition;
     private Quaternion carRotation;
+    private Vector3 pedPosition;
+    private Quaternion pedRotation;
 
     //Public variable for car prefab
     public GameObject carPrefab;
+    private GameObject pedPrefab;
+    public GameObject[] pedPrefabs;
 
     //Private variables for standard positions
     private float xSide = 15.0f;
@@ -28,20 +38,39 @@ public class SpawnManager : MonoBehaviour
     private float faceUp = 0.0f;
     private float faceDown = 180.0f;
 
-    //Private variable for width
-    private float roadWidth = 2.0f;
+    //Private variable for width and offset
+    private float roadWidth;
+    private float carOffset;
+    private float pedestrianOffset1;
+    private float pedestrianOffset2;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Create start positions and start rotations
-        startPositions = new Vector3[numPositions];
-        startRotations = new Quaternion[numPositions];
+        //Get road with to shift cars and pedestrians
+        roadWidth = GameObject.Find("RoadVertical").GetComponent<MeshRenderer>().bounds.size.x;
+        carOffset = roadWidth / 4;
+        pedestrianOffset1 = 3 * roadWidth / 4;
+        pedestrianOffset2 = roadWidth;
 
-        startPositions[0] = new Vector3(-xSide, yHeight, -roadWidth / 2);
-        startPositions[1] = new Vector3(xSide, yHeight, roadWidth / 2);
-        startPositions[2] = new Vector3(-roadWidth / 2, yHeight, zSide);
-        startPositions[3] = new Vector3(roadWidth / 2, yHeight, -zSide);
+        //Create start positions and start rotations
+        carStartPositions = new Vector3[carNumPositions];
+        startRotations = new Quaternion[numRotations];
+        pedStartPositions = new Vector3[pedNumPositions];
+
+        carStartPositions[0] = new Vector3(-xSide, yHeight, -carOffset);
+        carStartPositions[1] = new Vector3(xSide, yHeight, carOffset);
+        carStartPositions[2] = new Vector3(-carOffset, yHeight, zSide);
+        carStartPositions[3] = new Vector3(carOffset, yHeight, -zSide);
+
+        pedStartPositions[0] = new Vector3(-xSide, yHeight, -pedestrianOffset2);
+        pedStartPositions[1] = new Vector3(xSide, yHeight, pedestrianOffset2);
+        pedStartPositions[2] = new Vector3(-pedestrianOffset2, yHeight, zSide);
+        pedStartPositions[3] = new Vector3(pedestrianOffset2, yHeight, -zSide);
+        pedStartPositions[4] = new Vector3(-xSide, yHeight, pedestrianOffset1);
+        pedStartPositions[5] = new Vector3(xSide, yHeight, -pedestrianOffset1);
+        pedStartPositions[6] = new Vector3(pedestrianOffset1, yHeight, zSide);
+        pedStartPositions[7] = new Vector3(-pedestrianOffset1, yHeight, -zSide);
 
         startRotations[0] = Quaternion.Euler(0,faceRight, 0);
         startRotations[1] = Quaternion.Euler(0, faceLeft, 0);
@@ -49,7 +78,8 @@ public class SpawnManager : MonoBehaviour
         startRotations[3] = Quaternion.Euler(0, faceUp, 0);
 
         //Start spawning car every couple of seconds
-        InvokeRepeating("SpawnCarsRandom", delayStart, repeatingTime);
+        InvokeRepeating("SpawnCarsRandom", delayStart, carRepeatingTime);
+        InvokeRepeating("SpawnPedestriansRandom", delayStart, pedRepeatingTime);
     }
 
     // Update is called once per frame
@@ -61,10 +91,22 @@ public class SpawnManager : MonoBehaviour
     //Method to spawn cars at random positions
     private void SpawnCarsRandom()
     {
-        randomCarStart = Random.Range(0, startPositions.Length);
-        carPosition = startPositions[randomCarStart];
-        carRotation = startRotations[randomCarStart];
+        randomCarStart = Random.Range(0, carNumPositions);
+        randomRotation = randomCarStart;
+        carPosition = carStartPositions[randomCarStart];
+        carRotation = startRotations[randomRotation];
 
         Instantiate(carPrefab, carPosition, carRotation);
+    }
+
+    private void SpawnPedestriansRandom()
+    {
+        pedPrefab = pedPrefabs[Random.Range(0, pedPrefabs.Length)];
+        randomPedStart = Random.Range(0, pedNumPositions);
+        randomRotation = randomPedStart % 4;
+        pedPosition = pedStartPositions[randomPedStart];
+        pedRotation = startRotations[randomRotation];
+
+        Instantiate(pedPrefab, pedPosition, pedRotation);
     }
 }
